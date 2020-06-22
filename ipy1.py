71c39d631f81ab5167919fff24c16c526502b20c
anshumanojha <55602333+anshumanojha@users.noreply.github.com>
2020-06-22 11:42:08 +0530

    capstone project notebook
    about csapstone

---
+++ ipy1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is a jupyter notebook to be used in capstone project
+# 
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print ("hello capstone project")
+
+
+# In[ ]:
+
+
+
+
